4db3b24890a6157b1c515b376285a969fbdbb6c0
Mariano Rodriguez <marianojrodriguez10@gmail.com>
2022-07-07 03:57:44 -0300

    agregado de recursos libres

--- controllers/recursos.js
+++ controllers/recursos.js
@@ -1,6 +1,6 @@
 const { response } = require('express');
 const Recurso = require('../models/Recurso');
-
+const Reunion = require('../models/Reunion');
 /**
  * @method POST
  * @name crearRecurso
@@ -111,9 +111,85 @@ const eliminarRecurso = async (req, res = response) => {
   }
 };
 
+/**
+ * @method GET
+ * @name obtenerRecursosLibres
+ * @query { horaInicio:Date,
+ *  horaFinal:Date}
+ */
+const obtenerRecursosLibres = async (req, res = response) => {
+  let recursosOcupados = [];
+  let {
+    horaInicio,
+    horaFinal,
+  } = req.query;
+  let query = {};
+  if (horaInicio && horaFinal) {
+    query.horaInicio = { $gte: horaInicio };
+    query.horaFinal = { $lt: horaFinal };
+  } else {
+    return res.status(500).json({
+      status: 500,
+      message: 'no se envió fecha de Inicio o Fecha Final',
+    });
+  }
+  // verificar que la hora de inicio no sea mayor a hora de final
+  if (horaInicio > horaFinal) {
+    return res.status(500).json({
+      status: 500,
+      message: 'la hora de inicio no puede ser mayor a la hora de final',
+    });
+    return;
+  }
+  try {
+    let reuniones = await Reunion.find()
+      .populate('recursos');
+    let reunionesHorario = []
+    reuniones.forEach(r => {
+      //se parsean las horas en milisegundos para compararlas
+      if (Date.parse(r.horaInicio) >= Date.parse(horaInicio) && Date.parse(r.horaFinal) <= Date.parse(horaFinal)) {
+        //reuniones con ese rango de fecha y hora
+        reunionesHorario.push(r)
+      }
+    })
+    let recursosLibres = await Recurso.find();
+    reunionesHorario.forEach(r => {
+      r.recursos.forEach(e => {
+        //recorriendo las reuniones extraemos los recursos que están siendo ocupados en ese tiempo
+        recursosOcupados.push(e);
+      })
+    })
+    recursosOcupados.forEach(e => {
+      for (let index in recursosLibres) {
+        // console.log(recursosLibres[index]._id.toString())
+        // console.log(e._id.toString());
+        //se parsea a string los ids de los recursos para comparar
+        if (recursosLibres[index]._id.toString() == e._id.toString()) {
+          //a todos los recursos le sacamos los recursos ocupados y nos quedan los recursos libres
+          recursosLibres.splice(index, 1);
+        }
+      }
+    })
+
+    res.status(200).json({
+      status: 200,
+      data: { recursosLibres },
+
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 500,
+      message: 'internal server error',
+    });
+  }
+
+}
+
 module.exports = {
   crearRecurso,
   obtenerRecursos,
   modificarRecurso,
   eliminarRecurso,
+  obtenerRecursosLibres,
 };
