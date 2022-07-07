28039607d525f32c7b55482e2aa627a2b7972875
Mariano Rodriguez <marianojrodriguez10@gmail.com>
2022-07-07 17:25:26 -0300

    oficinas libres

--- controllers/oficina.js
+++ controllers/oficina.js
@@ -1,5 +1,7 @@
 const { response } = require('express');
 const Oficina = require('../models/Oficina');
+const Reunion = require('../models/Reunion');
+
 
 /**
  * @method POST
@@ -152,10 +154,85 @@ const eliminarOficina = async (req, res = response) => {
   }
 };
 
+/**
+ * @method GET
+ * @name obtenerOficinasLibres
+ * @query { horaInicio:Date,
+ *  horaFinal:Date}
+ */
+const obtenerOficinasLibres = async (req, res = response) => {
+  let oficinasOcupadas = [];
+  let {
+    horaInicio,
+    horaFinal,
+  } = req.query;
+  let query = {};
+  // if (horaInicio && horaFinal) {
+  //   query.horaInicio = { $gte: horaInicio };
+  //   query.horaFinal = { $lt: horaFinal };
+  // } else {
+  //   return res.status(500).json({
+  //     status: 500,
+  //     message: 'no se envió fecha de Inicio o Fecha Final',
+  //   });
+  // }
+  // verificar que la hora de inicio no sea mayor a hora de final
+  if (horaInicio > horaFinal) {
+    return res.status(500).json({
+      status: 500,
+      message: 'la hora de inicio no puede ser mayor a la hora de final',
+    });
+    return;
+  }
+  try {
+    let reuniones = await Reunion.find()
+      .populate('oficina');
+    let reunionesHorario = []
+    reuniones.forEach(r => {
+      //se parsean las horas en milisegundos para compararlas
+      if (Date.parse(r.horaInicio) >= Date.parse(horaInicio) && Date.parse(r.horaFinal) <= Date.parse(horaFinal)) {
+        //reuniones con ese rango de fecha y hora
+        reunionesHorario.push(r)
+      }
+    })
+    let oficinasLibres = await Oficina.find();
+    reunionesHorario.forEach(r => {
+      //recorriendo las reuniones extraemos las oficinas que están siendo ocupadas en ese tiempo
+      oficinasOcupadas.push(r.oficina);
+    })
+    oficinasOcupadas.forEach(e => {
+      for (let index in oficinasLibres) {
+        // console.log(recursosLibres[index]._id.toString())
+        // console.log(e._id.toString());
+        //se parsea a string los ids de los recursos para comparar
+        if (oficinasLibres[index]._id.toString() == e._id.toString()) {
+          //a todos los recursos le sacamos los recursos ocupados y nos quedan los recursos libres
+          oficinasLibres.splice(index, 1);
+        }
+      }
+    })
+
+    res.status(200).json({
+      status: 200,
+      data: { oficinasLibres },
+
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 500,
+      message: 'internal server error',
+    });
+  }
+
+}
+
+
 module.exports = {
   crearOficina,
   modificarOficina,
   obtenerOficinas,
   obtenerEstadisticasOficinas,
   eliminarOficina,
+  obtenerOficinasLibres,
 };
